c2098129082122e79bb73b41c53170543784b352
CharlesAlmeidaJr <jcharlesf.almeida@gmail.com>
2023-09-21 16:54:14 -0300

    Teste dado inicial

--- src/main/java/com/funtec/projetoFinalWebAPI/ProjetoFinalWebApiApplication.java
+++ src/main/java/com/funtec/projetoFinalWebAPI/ProjetoFinalWebApiApplication.java
@@ -1,13 +1,31 @@
 package com.funtec.projetoFinalWebAPI;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.funtec.projetoFinalWebAPI.model.FuncionarioInternoCargo;
+import com.funtec.projetoFinalWebAPI.model.form.FuncionarioInternoForm;
+import com.funtec.projetoFinalWebAPI.service.impl.FuncionarioInternoServiceImpl;
 
 @SpringBootApplication
 public class ProjetoFinalWebApiApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFinalWebApiApplication.class, args);
+		
+		
 	}
-
+//	@Bean
+//	CommandLineRunner init (FuncionarioInternoServiceImpl service) {
+//		return args -> {
+//			FuncionarioInternoForm funcionario = new FuncionarioInternoForm();
+//			funcionario.setNome("Admin");
+//			funcionario.setLogin("admin");
+//			funcionario.setSenha("admin");
+//			funcionario.setCargo(FuncionarioInternoCargo.GERENCIA);
+//			service.create(funcionario);
+//		};
+//	}
 }
