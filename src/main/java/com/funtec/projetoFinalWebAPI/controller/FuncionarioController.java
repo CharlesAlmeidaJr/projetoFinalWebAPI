39c4470840542e4f48b68f0545ff1ced4eb47d5a
CharlesAlmeidaJr <jcharlesf.almeida@gmail.com>
2023-09-24 14:52:45 -0300

    Iniciando adição do @CrossOrigin

--- src/main/java/com/funtec/projetoFinalWebAPI/controller/FuncionarioController.java
+++ src/main/java/com/funtec/projetoFinalWebAPI/controller/FuncionarioController.java
@@ -3,6 +3,7 @@ package com.funtec.projetoFinalWebAPI.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.funtec.projetoFinalWebAPI.service.impl.FuncionarioInternoServiceImpl;
 import com.funtec.projetoFinalWebAPI.service.impl.FuncionarioServiceImpl;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 	
